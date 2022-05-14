6c068fee2f0bc8e8d569254f620b867eb4b82f46
Lilan Manoj <lilanmanoj@adventus.io>
2022-05-15 00:17:24 +0530

    Added configuration bits to headre file

--- master.X/src/main.c
+++ master.X/src/main.c
@@ -5,8 +5,6 @@
  * Created on April 24, 2022, 4:14 PM
  */
 
-
-#include <xc.h>
 #include "../header/master.h"
 
 void main(void) {
--- slave.X/src/main.c
+++ slave.X/src/main.c
@@ -5,8 +5,6 @@
  * Created on April 24, 2022, 4:15 PM
  */
 
-
-#include <xc.h>
 #include "../header/slave.h"
 
 void main(void) {
